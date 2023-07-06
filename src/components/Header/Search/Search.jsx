6959b434f8e2b160a401faabb68adbc8b1e8eca6
Your Name <you@example.com>
2023-07-06 22:55:57 +0530

    knd

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { MdClose } from "react-icons/md";
 import "./Search.scss";
-import useFetch from "../../../hooks/useFetch";
 import { useNavigate } from "react-router-dom";
 
 const Search = ({ setSearchModal }) => {
@@ -12,13 +11,13 @@ const Search = ({ setSearchModal }) => {
         setQuery(e.target.value);
     };
 
-    let { data } = useFetch(
-        `/api/products?populate=*&filters[title][$contains]=${query}`
-    );
+    // let { data } = useFetch(
+    //     `/api/products?populate=*&filters[title][$contains]=${query}`
+    // );
 
-    if (!query.length) {
-        data = null;
-    }
+    // if (!query.length) {
+    //     data = null;
+    // }
 
     return (
         <div className="search-modal">
@@ -36,41 +35,13 @@ const Search = ({ setSearchModal }) => {
                 />
             </div>
             <div className="search-result-content">
-                {!data?.data?.length && (
+                {(
                     <div className="start-msg">
                         Start typing to see products you are looking for.
                     </div>
                 )}
                 <div className="search-results">
-                    {data?.data?.map((item) => (
-                        <div
-                            className="search-result-item"
-                            key={item.id}
-                            onClick={() => {
-                                navigate("/product/" + item.id);
-                                setSearchModal(false);
-                            }}
-                        >
-                            <div className="image-container">
-                                <img
-                                    src={
-                                        process.env
-                                            .REACT_APP_STRIPE_APP_DEV_URL +
-                                        item.attributes.image.data[0].attributes
-                                            .url
-                                    }
-                                />
-                            </div>
-                            <div className="prod-details">
-                                <span className="name">
-                                    {item.attributes.title}
-                                </span>
-                                <span className="desc">
-                                    {item.attributes.description}
-                                </span>
-                            </div>
-                        </div>
-                    ))}
+                    Working 
                 </div>
             </div>
         </div>
